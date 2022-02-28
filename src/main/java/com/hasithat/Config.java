98a265d09191a2db66bc7fa467ff959f6165b8e4
Hasitha Kankanige <hasithat@mobitel.lk>
2022-03-01 00:40:58 +0530

    mysql connection url changed

--- src/main/java/com/hasithat/Config.java
+++ src/main/java/com/hasithat/Config.java
@@ -1,8 +1,11 @@
 package com.hasithat;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = "com.hasithat.resource")
 @Configuration
 public class Config {
